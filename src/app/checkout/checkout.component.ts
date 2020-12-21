542ecafe7255d6fe2d844dbb90de1f7543bdb476
jasurkurbanovinit <jasurkurbanov96@gmail.com>
2020-12-21 11:38:39 +0500

    Input Added

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-checkout",
@@ -8,10 +9,27 @@ import { CartService } from "../cart.service";
 })
 export class CheckoutComponent implements OnInit {
   data;
+  checkoutForm;
 
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      name: "",
+      address: ""
+    });
+  }
 
   ngOnInit() {
     this.data = this.cartService.getItems();
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    this.data = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn("Your order has been submitted", customerData);
   }
 }
